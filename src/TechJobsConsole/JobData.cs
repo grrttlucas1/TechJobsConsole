19dd9e8a83c5923439f1c25a70e1d6f4665801ba
Garrett Lucas <grrtt_lucas@yahoo.com>
2017-03-06 22:22:28 -0600

    Completed TechJobs Console assignment

--- src/TechJobsConsole/JobData.cs
+++ src/TechJobsConsole/JobData.cs
@@ -37,7 +37,25 @@ namespace TechJobsConsole
             }
             return values;
         }
+        
+        public static List<Dictionary<string, string>> FindByValue(string value)
+        {
+            LoadData();
 
+            List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>();
+
+            foreach (Dictionary<string, string> row in AllJobs)
+            {
+                foreach (KeyValuePair<string, string> kvpair in row)
+                {
+                    if (kvpair.Value.ToLower().Contains(value.ToLower()))
+                    {
+                        jobs.Add(row);
+                    }
+                }
+            }
+            return jobs;
+        }
         public static List<Dictionary<string, string>> FindByColumnAndValue(string column, string value)
         {
             // load data, if not already loaded
@@ -49,7 +67,7 @@ namespace TechJobsConsole
             {
                 string aValue = row[column];
 
-                if (aValue.Contains(value))
+                if (aValue.ToLower().Contains(value.ToLower()))
                 {
                     jobs.Add(row);
                 }
--- src/TechJobsConsole/Program.cs
+++ src/TechJobsConsole/Program.cs
@@ -41,7 +41,7 @@ namespace TechJobsConsole
                     else
                     {
                         List<string> results = JobData.FindAll(columnChoice);
-
+                        results.Sort();
                         Console.WriteLine("\n*** All " + columnChoices[columnChoice] + " Values ***");
                         foreach (string item in results)
                         {
@@ -63,12 +63,27 @@ namespace TechJobsConsole
                     // Fetch results
                     if (columnChoice.Equals("all"))
                     {
-                        Console.WriteLine("Search all fields not yet implemented.");
+                        searchResults = JobData.FindByValue(searchTerm);
+                        if (searchResults.Count == 0)
+                        {
+                            Console.WriteLine("\nEntry not found");
+                        }
+                        else
+                        {
+                            PrintJobs(searchResults);
+                        }
                     }
                     else
                     {
                         searchResults = JobData.FindByColumnAndValue(columnChoice, searchTerm);
-                        PrintJobs(searchResults);
+                        if (searchResults.Count == 0)
+                        {
+                            Console.WriteLine("\nEntry not found");
+                        }
+                        else
+                        {
+                            PrintJobs(searchResults);
+                        }
                     }
                 }
             }
@@ -118,7 +133,15 @@ namespace TechJobsConsole
 
         private static void PrintJobs(List<Dictionary<string, string>> someJobs)
         {
-            Console.WriteLine("printJobs is not implemented yet");
+            foreach ( Dictionary<string, string> myDict in someJobs ) 
+            {
+                Console.WriteLine("*****");
+                foreach (KeyValuePair<string, string> kvpair in myDict)
+                {
+                    Console.WriteLine(string.Format("{0}: {1}", kvpair.Key, kvpair.Value));
+                }
+                Console.WriteLine("*****\n");
+            }
         }
     }
 }
